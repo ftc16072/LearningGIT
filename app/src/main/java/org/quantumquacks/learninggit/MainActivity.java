1622446eb281e45565fdac437ab37781fbef4d4a
Jordan Liggitt <jordan@liggitt.net>
2020-08-24 19:35:31 -0400

    Procrastinate on amazing plans

--- app/src/main/java/org/quantumquacks/learninggit/MainActivity.java
+++ app/src/main/java/org/quantumquacks/learninggit/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: do awesome things
+        // TODO: do awesome things, tomorrow
     }
 }
